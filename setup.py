b0595d4a192eecfb3d5ced47b40c8ae9048bd50d
Tyson Holub <tholub@mobiusworks.com>
2018-05-15 14:54:22 -0400

    add support to render README.md on PyPi, PyPi bump to v0.4.5

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
+import os
 from setuptools import setup, find_packages
 
-__version__ = '0.4.4'
+__version__ = '0.4.5'
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name='RestResponse',
     version=__version__,
     description='A fluent python object for interfacing with RESTful JSON APIs.',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     url='https://github.com/MobiusWorksLLC/RestResponse.git',
     author='Tyson Holub',
     author_email='tholub@mobiusworks.com',
